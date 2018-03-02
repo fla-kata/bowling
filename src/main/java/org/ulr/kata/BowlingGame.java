c09c44cd0336d73f12cd66473d4a103dac93e408
Francois Lagoueyte <francois.lagoueyte@asso-cocktail.fr>
2018-03-02 22:41:48 +0100

    init exigences+structure bowlinggame

---
+++ src/main/java/org/ulr/kata/BowlingGame.java
@@ -0,0 +1,12 @@
+package org.ulr.kata;
+
+/**
+ * Exigences :
+ * Ecrire deux méthodes :
+ * 1- une methode roll(pins : int) où pins = nombre de quilles tombées
+ * 2- score(): int laquelle est appelée à la fin de la partie et retourne le score
+ */
+public class BowlingGame {
+
+
+}
---
+++ src/test/java/org/ulr/kata/BowlingGameTest.java
@@ -0,0 +1,7 @@
+package org.ulr.kata;
+
+import static org.junit.Assert.*;
+
+public class BowlingGameTest {
+
+}
